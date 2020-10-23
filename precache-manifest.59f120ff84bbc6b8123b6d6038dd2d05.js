c702f758faa69b7997e4635d395967e069248bc3
911harm <911harm@gmail.com>
2020-10-23 00:02:03 -0500

    Updates

---
+++ precache-manifest.59f120ff84bbc6b8123b6d6038dd2d05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a0a05f995b83d37ad42d3d914aecea",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "56a71d20a6c29bca9afb",
+    "url": "/pokedex/static/js/2.f468d2d2.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/pokedex/static/js/2.f468d2d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52864f1155d848aac9a4",
+    "url": "/pokedex/static/js/main.0cd80705.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
